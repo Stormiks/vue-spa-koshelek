8de4eb47020cbbcb4d653662b86ad13de478829a
Skylinker <skylinkertu33@gmail.com>
2021-04-14 10:44:22 +0700

    Added folder dist

---
+++ dist/assets/js/chunk-2d0b91da.80bbe319.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b91da"],{3227:function(n,e,r){"use strict";r.r(e);var t=function(){var n=this,e=n.$createElement,r=n._self._c||e;return r("h1",[n._v("Error page 404")])},a=[],u={name:"ErrorLayout"},o=u,c=r("2877"),l=Object(c["a"])(o,t,a,!1,null,null,null);e["default"]=l.exports}}]);
